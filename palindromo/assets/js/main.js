1979f3b26e1488f9e6d9e4aa440abb06581bce67
matteoga95 <gallo.work95@gmail.com>
2022-10-14 12:13:39 +0200

    fix no rgx solo invertendo l'ordine delle lettere

--- palindromo/assets/js/main.js
+++ palindromo/assets/js/main.js
@@ -4,19 +4,15 @@
 const userWord = prompt("Inserisci la parola da verificare: ")
 
 
-
 function palindrome(str) {
-    // Step 1. Lowercase la stringa e uso delle rgx per rimuovere caratteri non desiderati
-    var re = /[\W_]/g;
+  
     //tolgo gli eventuali caratteri non desiderati dalla stringa come  punti caratteri speciali etc etc
-    var lowRegStr = str.toLowerCase().replace(re, '');
+    var lowRegStr = str.toLowerCase()
 
-
-    // Step 2. uso i metodi delle stringhe per dividere la stringa con lo split, invertirla di ordine, e poi riunirla tutta in ordine invertito
+    //uso i metodi delle stringhe per dividere la stringa con lo split, invertirla di ordine, e poi riunirla tutta in ordine invertito
     var reverseStr = lowRegStr.split('').reverse().join('');
 
-
-    // Step 3. Controllo se la stringa passata come parametro è uguale a quella invertita sopra e ritorno il valore true o false
+    //  Controllo se la stringa passata come parametro è uguale a quella invertita sopra e ritorno il valore true o false
     return reverseStr === lowRegStr;
 }
 
